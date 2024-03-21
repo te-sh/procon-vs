e7eae82255579f7649a9b26af526e76f228fd015
te-sh <admire.japan@gmail.com>
2024-03-22 03:14:24 +0900

    implement procon.copy

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 const vscode = require('vscode');
+const fs = require('fs');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -7,14 +8,47 @@ function activate(context) {
 	console.log('Congratulations, your extension "procon" is now active!');
 
 	let disposable = vscode.commands.registerCommand('procon.copy', function () {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const text = editor.document.getText();
-			const lines = text.split("\n");
+		const workspaceDir = vscode.workspace.workspaceFolders[0].uri.path;
+		const libDir = workspaceDir + '/code/lib';
 
-			const out = lines.join("\n")
-			vscode.env.clipboard.writeText(out)
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
 		}
+		
+		const document = editor.document;
+		const text = document.getText();
+		const lines = text.split('\n');
+
+		for (;;) {
+			let hit = false;
+			lines.some((line, index) => {
+				const m = line.match(/^require "(.*)"/);
+				if (!m) {
+					return;
+				}
+
+				const file = m[1];
+				if (!file.startsWith('procon')) {
+					return;
+				}
+
+				hit = true;
+				const lib = fs.readFileSync(libDir + '/' + file + '.cr', 'utf-8');
+				const libLines = lib.split('\n').filter((libLine) => {
+					return !libLine.match(/^\s*#/);
+				});
+
+				lines.splice(index, 1, ...libLines);
+				return true;
+			});
+			if (!hit) {
+				break;
+			}
+		}
+
+		const out = lines.join('\n');
+		vscode.env.clipboard.writeText(out);
 	});
 
 	context.subscriptions.push(disposable);
